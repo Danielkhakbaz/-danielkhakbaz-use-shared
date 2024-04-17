91c26a4d01f6d8d40e6c3f3512daac23a3974e80
Daniel Khakbaz <danialkhakbaz.dk@gmail.com>
2024-04-17 13:49:39 +0330

    (feat): Add intellisense to the package

--- lib/indes.d.ts
+++ lib/indes.d.ts
@@ -1,5 +1,19 @@
 import { ReactNode } from "react";
 
+/**
+ * Function 1.
+ * 
+ * @param arg1 {string} The first argument, a string value.
+ * @param arg2 {number} The second argument, a number value.
+ * @returns {boolean} True if the string length is greater than the number, false otherwise.
+ */
 export function SharedProvider(state: unknown, children: ReactNode): unknown;
 
+/**
+ * Function 2.
+ * 
+ * @param arg1 {string} The first argument, a string value.
+ * @param arg2 {number} The second argument, a number value.
+ * @returns {boolean} True if the string length is greater than the number, false otherwise.
+ */
 export function useShared(): unknown;
